3c00e1e0d686caa3e568943c2b213044f27348d2
nidhik3 <nidhikatkani3.com>
2024-08-12 23:17:29 +0530

    saas

---
+++ application/views/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>AI SAAS</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon"> -->
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+    rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/responsive.css">
+
+  <!-- =======================================================
+  * Template Name: Butterfly
+  * Template URL: https://bootstrapmade.com/butterfly-free-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between md-pl-5">
+
+      <a href="Main font" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid">
+        ai dynamics</a>
+
+  
+
+
+      <nav id="navbar" class="navbar">
+
+        <ul>
+          <li><a class="nav-link scrollto active" href="Main">Home</a></li>
+          <li><a class="nav-link scrollto" href="">Solutions</a></li>
+          <li><a class="nav-link scrollto" href="">Cases</a></li>
+          <li><a class="nav-link scrollto " href="">Blogs</a></li>
+          <li><a class="nav-link scrollto" href="Career">Pricing</a></li>
+          <li><a class="nav-link scrollto" href="">About</a></li>
+          <li><a class="nav-link scrollto" href="">Contact</a></li>
+          <li><a class="nav-link scrollto " href="">Buy template</a></li>
+
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> -->
+          <!-- <li><a class="nav-link scrollto blue" href="Contact">Contact</a></li> -->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+
+
+</body>
+
+</html>
---
+++ application/views/main.php
@@ -0,0 +1,67 @@
+<style>
+
+.building {
+    position: relative;
+}
+
+.text1 {
+    position: absolute;
+    top: 8%;
+    left: 53px;
+    color: #fff; /* Text color for contrast */
+    padding: 15px; /* Padding for some space around the text */
+    border-radius: 10px; /* Optional rounded corners */
+}
+
+
+
+</style>
+
+<section style=" background-image: url('assets/img/leaf.png'); background-repeat: no-repeat;">
+<div class="container content " style="padding-top: 160px;color: black;">
+        <p class="font" style="font-size: 65px;">Transform Your Business with Innovative Tech Solutions</p>
+        <div class="row">
+            <div class="col-md-12 building">
+                <img src="assets/img/img1.avif" alt="" style="height: 90%; width: 100%; border-radius: 20px">
+                <div class="col-md-5 text1">
+                    <h4 class="heading font">Empowering Small Businesses And Startups To Thrive In The Digital Age</h4>
+                    <hr style="opacity: 2.25;">
+                    <p class=""style="font-family: Dosis, sans-serif;">Welcome to AI Dynamics, where we specialize in helping small to medium-sized non-tech companies and tech startups streamline their operations with cutting-edge technology. Our expert team is dedicated to providing innovative solutions that drive growth and efficiency for your business.</p>
+                    <div class="pt-3">
+                        <a class="black " href="">Learn More</a>
+                        <a class="white ml-3 " href="" style=" margin-left: 10px;">Contact us</a>
+                    </div>
+                   
+                </div>
+              
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="noTop">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <p class="mb-0">Trusted by
+                </p>
+                <p>500+ companies
+                </p>
+            </div>
+            <div class="col-md-10"></div>
+        </div>
+    </div>
+</section>
+
+<section class="space" style="background-color: rgb(247, 247, 247);">
+    <div class="container">
+        <div class="row">
+            <p class="heading font">
+                AI Dynamics is dedicated to providing AI-powered technology solutions that drive growth and efficiency for businesses of all sizes.
+            </p>
+        </div>
+    </div>
+
+</section>
+
+   
